62cd7d95b514880ca68ac8bd3104cb009837fe33
Pavlo4242 <pavlostepanchuk15@gmail.com>
2025-11-27 17:29:18 +0700

    Update main.rs

--- crates/gui/src/main.rs
+++ crates/gui/src/main.rs
@@ -3,83 +3,14 @@ use rfd::FileDialog;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
-// Your producer trait and implementation
-trait Producer {
-    fn next(&mut self) -> Result<Option<Vec<u8>>, String>;
-    fn size(&self) -> usize;
-}
-use super::Producer;
+use rfd::FileDialog;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
-pub struct DefaultQuery {
-    min_length: u32,
-    max_length: u32,
-    current: Vec<u8>,
-    char_set: Vec<u8>,
-    rolled: bool,
-}
+// Use the Producer trait and DefaultQuery struct from the producer crate
+use producer::{Producer, default_query::DefaultQuery};
 
-impl DefaultQuery {
-    pub fn new(max_length: u32, min_length: u32) -> Self {
-        let mut char_set: Vec<u8> = (b'0'..=b'9')
-            .chain(b'A'..=b'Z')
-            .chain(b'a'..=b'z')
-            .chain(b'!'..=b'/')
-            .chain(b':'..=b'@')
-            .chain(b'['..=b'`')  // FIXED: Changed from b'' to b'`'
-            .chain(b'{'..=b'~')
-            .collect();
-
-        char_set.sort();
-        
-        Self {
-            max_length,
-            min_length,
-            current: vec![char_set[0]; min_length as usize],
-            char_set,
-            rolled: false,
-        }
-    }
-}
-
-impl Producer for DefaultQuery {
-    fn next(&mut self) -> Result<Option<Vec<u8>>, String> {
-        let mut stopped = false;
-        for i in 0..self.current.len() {
-            let spot = match self.char_set.binary_search(&self.current[i]) {
-                Ok(spot) => spot,
-                Err(_) => return Err("Couldn't find character in character set".to_string()),
-            };
-            if spot >= self.char_set.len() - 1 {
-                self.current[i] = self.char_set[0];
-            } else {
-                self.current[i] = self.char_set[spot + 1];
-                stopped = true;
-                break;
-            }
-        }
-        if !stopped {
-            self.current.insert(0, self.char_set[0]);
-            if self.current.len() > self.max_length as usize {
-                if self.rolled {
-                    return Err("Out of elements".to_string());
-                } else {
-                    self.rolled = true;
-                    return Ok(Some(self.current.clone()));
-                }
-            }
-        }
-        let return_value = self.current.clone();
-        Ok(Some(return_value))
-    }
-
-    fn size(&self) -> usize {
-        let mut ret = 0usize;
-        for len in self.min_length..=self.max_length {
-            ret += self.char_set.len().pow(len as u32);
-        }
-        ret
-    }
-}
+#[derive(Default)]
 
 #[derive(Default)]
 struct PdfCrackerApp {
