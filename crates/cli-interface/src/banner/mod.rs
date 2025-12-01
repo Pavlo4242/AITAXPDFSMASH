5478fdd3af80808bcf2cab1bb97996a2b5e7cc49
Pavlo4242 <pavlostepanchuk15@gmail.com>
2025-12-01 12:58:18 +0700

    Update mod.rs

--- crates/cli-interface/src/banner/mod.rs
+++ crates/cli-interface/src/banner/mod.rs
@@ -5,6 +5,10 @@ pub fn banner() {
     control::set_virtual_terminal(true).unwrap();
 
     println!(
+    use colored::*;
+#[cfg(windows)]
+pub fn banner() {
+    println!(
         "{}",
         format!(include_str!("banner.txt"), env!("CARGO_PKG_VERSION"))
             .bold()
